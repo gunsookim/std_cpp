8bfff3ebdea316bb9aef038b61a3c9be9ca754f5
gunsookim <soo85925@naver.com>
2022-09-08 20:21:46 +0900

    xcode commit test

---
+++ test/test/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by 김건수 on 2022/09/08.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
