21d424143fcbc278f6b7d1e1aae771f77bd383db
gunsookim <soo85925@naver.com>
2022-10-13 23:51:59 +0900

    cpp_til

---
+++ test/test/data_type.cpp
@@ -0,0 +1,16 @@
+//
+//  data_type.cpp
+//  c_practice
+//
+
+#include <stdio.h>
+int main(void){
+    int a;
+    printf("char형 크기: %lu\n", sizeof(char));
+    printf("int형 크기: %lu\n", sizeof(int));
+    printf("short형 크가: %lu\n", sizeof(short));
+    printf("long형 크기: %lu\n", sizeof(long));
+    printf("float형 크기: %lu\n", sizeof(float));
+    printf("double형 크기: %lu\n", sizeof(double));
+    return 0;
+}
