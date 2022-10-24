5bfde02c69bd9d9cbf995811754e6266c9d8d73a
gunsookim <soo85925@naver.com>
2022-10-24 13:20:18 +0900

    c연습문제

---
+++ test/test/for2.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void){
+    int n1, n2;
+    scanf("%d %d", &n1, &n2);
+    for(int i = 0 ; i<n1;i++){
+        printf("%d", n1);
+    }
+    printf("\n");
+    for(int i = 0 ; i<n2;i++){
+        printf("%d", n2);
+    }
+    return 0;
+}
